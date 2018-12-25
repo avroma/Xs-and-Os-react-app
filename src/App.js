baed02550416b38217e7731c904c75f8e72e5f63
avroma <avromanov17@gmail.com>
2018-12-25 23:10:01 +0300

    make minor fixes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import './App.css'
 import Game from './components/game'
 
 class App extends Component {
   render() {
     return (
-      <div style={{marginTop: '5%'}}>
+      <div className='App'>
         <Game />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
